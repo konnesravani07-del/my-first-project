1003f993395eccf9eab2b3a101ea4c231c16ebc1
SRAVANI KONNE <konnesravani07@gmail.com>
2026-04-10 21:11:37 +0530

    Student Result App

---
+++ Main.java
@@ -0,0 +1,217 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Hello from java");
+        //System.out.println("My name is Sravani");
+        //System.out.println("I am learning Java");
+        //String name = "Sravani";
+        //int age = 24;
+
+        //System.out.println(name);
+       // System.out.println(age);
+       //System.out.println("My Name is " + name );
+       //System.out.println("My Age " + age);
+       //Scanner sc = new Scanner(System.in);
+
+       //System.out.println("Enter your Name: ");
+       //String name = sc.nextLine();
+
+       //System.out.println("Hello " + name);
+       
+       //sc.close();
+
+       //String name = "Ambadas";
+       //int age = 25;
+
+       //System.out.println("Name: " + name);
+       //System.out.println("Age: " + age);
+       //int a = 10;
+       //int b = 20;
+       //System.out.println(" Sum is " + (a + b));
+       //double price = 99.5;
+       //System.out.println("Price is " + price);
+       //char grade = 'A';
+
+    //System.out.println("Grade is " + grade);
+    //String name = "Sravani";
+    //int age = 24;
+   // double height = 5.0;
+   // System.out.println("My name is " + name);
+    //System.out.println("I am " + age + " years old");
+   // System.out.println("My height is " + height + " feet");
+   //int a = 15;
+   //int b = 5;
+
+   //System.out.println("Addition " + (a + b));
+   //System.out.println("Subtraction " + (a-b));
+   //System.out.println("Multiplication " + (a*b));
+   //System.out.println("Division " + (a/b));
+   //int num = 7;
+   //System.out.println("Square is " + (num*num));
+   //System.out.println("Cube is " + (num*num*num));
+   //int num = 10;
+   //System.out.println("Number is " + num);
+   //System.out.println("Double is " + (num*2));
+  // System.out.println("Half is " + (num/2));
+  //int a = 8;
+  //int b = 3;
+  //System.out.println("Sum is " + (8 + 3));
+  //System.out.println("Diffrence is " + (8 - 3));
+  //System.out.println("Product is " + (8 * 3));
+  //System.out.println("Quotient is " + (8 / 3));
+  //System.out.println("Remainder is " + (8 % 3));
+  //int num = 10;
+  //System.out.println("Square is " + (num*num));
+  //System.out.println("Is even: " + (num % 2 == 0));
+   //int num =10;
+  //System.out.println("Is even: " + (num % 2 == 0));
+  //int num = 7;
+   //if(num % 2 == 0) {
+    //System.out.println("Number is Even");
+  // }
+   //else {
+   // System.out.println("Number is Odd");
+   //}
+//    Scanner sc = new Scanner(System.in);
+//    System.out.println("Enter Number: " );
+//    int num = sc.nextInt();
+//    if(num % 2 == 0){
+//     System.out.println("Number is Even");
+//    } else {
+//     System.out.println("Number is Odd");
+//     sc.close();
+//    }
+// Scanner sc = new Scanner(System.in);
+// System.out.println("Enter Number: ");
+// int num = sc.nextInt();
+// if(num > 0) {
+//     System.out.println("Positive");
+// } else if (num <0) {
+//     System.out.println("Negitive");
+// } else {
+//     System.out.println("Zero");
+//     sc.close();
+// }
+// Scanner sc = new Scanner(System.in);
+//  System.out.println("Enter Number: ");
+//  int num = sc.nextInt();
+//  if(num > 5) {
+//     System.out.println("Positive");
+//  } else if (num < -2) {
+//     System.out.println("Negitive");
+//  } else {
+//     System.out.println("Zero");
+//     sc.close();
+//  }
+// Scanner sc = new Scanner(System.in);
+// System.out.println("Enter First Number");
+// int a = sc.nextInt();
+
+// System.out.println("Enter Second Number");
+// int b = sc.nextInt();
+
+// if(a > b) {
+//    System.out.println("Largest Number Is: " + a);
+// } else if(a < b){
+//    System.out.println("Largest Number is: " + b);
+//    sc.close();
+// }
+// Scanner sc = new Scanner(System.in);
+// System.out.println("Enter Number: ");
+// int num = sc.nextInt();
+// if(num % 2 ==0) {
+//    System.out.println("Even Number");
+// } else {
+//    System.out.println("Odd Number");
+//    sc.close();
+// Scanner sc = new Scanner(System.in);
+// // System.out.println("Positive ");
+// // System.out.println("Negitive ");
+// // System.out.println("Zero");
+// int num = sc.nextInt();
+// if(num > 0) {
+//    System.out.println("Positive ");
+// } else if(num < 0){
+//    System.out.println("Negitive");
+// } else {
+//    System.out.println("Zero");
+//    sc.close();
+// }
+// Scanner sc = new Scanner(System.in);
+// int num = sc.nextInt();
+// if(num % 5 == 0){
+//    System.out.println("Divisible by 5 ");
+// } else {
+//    System.out.println("Not divisible by 5 ");
+//    sc.close();
+// }
+// Scanner sc = new Scanner(System.in);
+// int num = sc.nextInt();
+// if(num % 3 == 0 && num % 5 == 0) {
+//    System.out.println("Multiple of both 3 and 5 ");
+// } else if(num % 3 == 0){
+//    System.out.println("Only multiple of 3");
+
+// } else if(num % 5 == 0){
+//    System.out.println("Only multiple of 5");
+// } else {
+//    System.out.println("Not a multiple of 3 and 5");
+//    sc.close();
+// }
+// Scanner sc = new Scanner(System.in);
+// int num = sc.nextInt();
+// if(num % 2 == 0 && num % 3 == 0){
+//    System.out.println("Divisible by 2 and 3" );
+// } else if(num % 2 == 0) {
+//    System.out.println("Only 2");
+// } else if (num % 3 == 0){
+//    System.out.println("Only 3");
+// } else {
+//    System.out.println("None");
+// }
+// Scanner sc = new Scanner(System.in);
+// int num = sc.nextInt();
+// if(num >= 90) {
+//    System.out.println('A');
+// } else if(num >= 75){
+//    System.out.println("B");
+// } else if(num >= 50){
+//    System.out.println('C');
+// }else {
+//    System.out.println("Fail");
+// }
+// Scanner sc = new Scanner(System.in);
+//  int a = sc.nextInt();
+//  int b = sc.nextInt();
+//  int c = sc.nextInt();
+// if(a > b && a > c){
+//    System.out.println("a");
+// } else if(b > a && b > c){
+//    System.out.println("b");
+// } else {
+//    System.out.println("c");
+// }
+Scanner sc = new Scanner(System.in);
+int num = sc.nextInt();
+if (num == 0){
+   System.out.println("Zero");
+} else if(num % 2 == 0){
+   System.out.println("even && positive");
+} else if( num > 0){
+   System.out.println("even && negative");
+} else if (num % 2 == 0 && num> 0){
+   System.out.println("odd && positive");
+} else {
+   System.out.println("odd && negative");
+}
+
+}
+}
+
+
+
+
+
+
+
+    
